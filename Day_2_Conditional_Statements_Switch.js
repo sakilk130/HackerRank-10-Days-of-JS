6aa88f297780e1565eb435f8f7cd4ef3c754fb36
Sakil Khan <sakilk130@gmail.com>
2020-12-15 23:16:02 +0600

    Day_2

---
+++ Day_2_Conditional_Statements_Switch.js
@@ -0,0 +1,52 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', (inputStdin) => {
+  inputString += inputStdin;
+});
+
+process.stdin.on('end', (_) => {
+  inputString = inputString
+    .trim()
+    .split('\n')
+    .map((string) => {
+      return string.trim();
+    });
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+function getLetter(s) {
+  let letter;
+  // Write your code here
+  switch (s[0]) {
+    case 'a' || 'e' || 'i' || 'o' || 'u':
+      letter = 'A';
+      break;
+    case 'b' || 'c' || 'd' || 'f' || 'g':
+      letter = 'B';
+      break;
+    case 'h' || 'j' || 'k' || 'l' || 'm':
+      letter = 'C';
+      break;
+    //case('n'||'p'||'q'||'r'||'s'||'t'||'v'||'w'||'x'||'y'||'z'):
+    default:
+      letter = 'D';
+  }
+  return letter;
+}
+
+function main() {
+  const s = readLine();
+
+  console.log(getLetter(s));
+}
